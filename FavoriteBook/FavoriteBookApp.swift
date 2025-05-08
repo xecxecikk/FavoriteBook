970364b7b6a297ab8dc45282dd9bee0b7d0e08b6
xecxecikk <xecekrdnz_94@outlook.com>
2025-05-08 21:27:54 +0300

    Initial Commit

---
+++ FavoriteBook/FavoriteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteBookApp.swift
+//  FavoriteBook
+//
+//  Created by XECE on 8.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
