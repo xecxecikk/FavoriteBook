039ffc6b357be70a37461232413b936ecabed158
xecxecikk <xecekrdnz_94@outlook.com>
2025-05-09 12:13:48 +0300

    first commit

---
+++ FavoriteBook/DetailsView.swift
@@ -0,0 +1,8 @@
+//
+//  DetailsView.swift
+//  FavoriteBook
+//
+//  Created by XECE on 8.05.2025.
+//
+
+import Foundation
---
+++ FavoriteBook/FavoriteModel.swift
@@ -0,0 +1,8 @@
+//
+//  FavoriteModel.swift
+//  FavoriteBook
+//
+//  Created by XECE on 8.05.2025.
+//
+
+import Foundation
